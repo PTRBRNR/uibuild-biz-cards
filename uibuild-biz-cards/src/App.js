884c0a3d6b6295f74f02fe614ebfe87a7ff80192
PTRBRNR <ptrbnr@rocketmail.com>
2022-02-08 11:43:35 +0100

    :tada:

---
+++ uibuild-biz-cards/src/App.js
@@ -0,0 +1,11 @@
+import Sitebuild from "./components/Sitebuild";
+
+function App() {
+  return (
+    <div className="App">
+      <Sitebuild />
+    </div>
+  );
+}
+
+export default App;
---
+++ uibuild-biz-cards/src/components/Sitebuild.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+function Sitebuild() {
+  return (
+    <div className="container">
+      <div className="divek">
+        <h2>John Freeman</h2>
+        <h6>PIZZA THIEF</h6>
+        <div>
+          <p>2610 NW Vaughn St, Portland,</p>
+          <p>
+            OR 97210, <br /> United States
+          </p>
+        </div>
+      </div>
+      <div className="divek">
+        <h2>Angela Sliders</h2>
+        <h6>MAX STEAKS</h6>
+        <div>
+          <p>3653 Germantown Ave, Philadelphia,</p>
+          <p>
+            PA 19140, <br /> United States
+          </p>
+        </div>
+      </div>
+      <div className="divek">
+        <h2>
+          Rita <br />
+          Lehigh
+        </h2>
+        <h6>PIZZA RANCH</h6>
+        <div>
+          <p>1327 Main St Ste. 6, Billings Heights,</p>
+          <p>
+            MT59105, <br /> United States
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Sitebuild;
